f98aeb275632967a95d2fe5f61cc90b1dbaa97a5
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-18 20:14:20 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,16 @@
 # Write your code here
+count = 10
+
+while count >= 1 do
+    puts count
+    count -= 1
+end
+  puts "Happy New Year!"
+
+
+  # count = 10
+  # while count >= 1 do
+  #   puts count
+  #   count -= 1
+  # end
+  # puts "Happy New Year!"
